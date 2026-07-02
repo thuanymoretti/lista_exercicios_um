3c2c07d27595de39b3c13b98cf0ec085ad175455
thuany_moretti <thuany_moretti@estudante.sesisenai.org.br>
2026-07-02 08:57:48 -0300

    add - finalizei exercício 2

---
+++ ex_02.php
@@ -0,0 +1,14 @@
+<?php
+
+// Inverter o texto
+
+function inverterTexto($texto) {
+    
+    $textoInvertido = strrev($texto);
+    return $textoInvertido;
+
+}
+
+echo "Texto original: Oi, mundo! <br>";
+echo "Texto invertido: " . inverterTexto("Oi, mundo!") . "<br>";
+echo "Numero de caracteres: " . strlen("Oi, mundo!");
