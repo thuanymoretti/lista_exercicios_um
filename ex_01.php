928b52523fb7d47991663c5c3a2534cf20ecba04
thuany_moretti <thuany_moretti@estudante.sesisenai.org.br>
2026-07-02 08:34:29 -0300

    add - finalizei a um

--- ex_01.php
+++ ex_01.php
@@ -1,1 +1,22 @@
 <?php
+
+// (x² + y²) / (x + y)
+
+function calcularFormula($x, $y)
+{
+    if (($x +$y) == 0) {
+        return "Não é possivel realizar a divisão por zero.";
+    }
+
+    $resultado = (pow($x, 2) + pow($y, 2)) / ($x + $y);
+
+    return $resultado;
+}
+
+$x = 10;
+$y = 5;
+
+echo "Valor de X: $x <br>";
+echo "Valor de Y: $y <br><br>";
+echo "Resultado: " . calcularFormula($x, $y);
+
