4eb75b93237e6678ecba7486f1e136fd34847940
thuany_moretti <thuany_moretti@estudante.sesisenai.org.br>
2026-07-07 07:41:01 -0300

    add - finalizei exercício 4

---
+++ ex_04.php
@@ -0,0 +1,9 @@
+<?php
+
+function gerarSenha($senha) {
+    $caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*';
+    
+    return substr(str_shuffle($caracteres), 0, $senha);
+}
+
+echo "Senha gerada: " . gerarSenha(10);
